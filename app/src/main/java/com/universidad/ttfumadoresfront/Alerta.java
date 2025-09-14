449610044df1fc0b21dc2334bf727265d75e01c0
cNava03 <147421034+cnava03@users.noreply.github.com>
2025-09-14 14:22:08 -0600

    Commit

---
+++ app/src/main/java/com/universidad/ttfumadoresfront/Alerta.java
@@ -0,0 +1,14 @@
+package com.universidad.ttfumadoresfront;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Alerta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alerta);
+    }
+}
---
+++ app/src/main/java/com/universidad/ttfumadoresfront/Registrar_cuenta.java
@@ -0,0 +1,15 @@
+package com.universidad.ttfumadoresfront;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registrar_cuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_cuenta);
+    }
+}
+
